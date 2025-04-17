763f3cf943a18d445d1e7ff424d91916d9d331e7
ibrqp <mibrahimalamin98@gmail.com>
2025-04-17 08:12:45 +0700

    Tambah fitur baru

---
+++ src/Mahasiswa.java
@@ -0,0 +1,17 @@
+public class Mahasiswa {
+
+    String NIM, nama, prodi;
+
+    public Mahasiswa(){
+
+    }
+    public Mahasiswa(String NIM, String nama, String prodi ) {
+        this.NIM = NIM;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+
+    void tampilMahasiswa(){
+        
+    }
+}
---
+++ src/MataKuliah.java
@@ -0,0 +1,21 @@
+package ALSD.src;
+
+public class MataKuliah {
+    String KodeMK, NamaMK;
+    int sks;
+
+    public MataKuliah() {
+    }
+
+    
+    public MataKuliah(String KodeMK, String NamaMK, int sks) {
+        this.KodeMK = KodeMK;
+        this.NamaMK = NamaMK;
+        this.sks = sks;
+    }
+
+    void tampilMatakuliah(){
+        
+    }
+    
+}
